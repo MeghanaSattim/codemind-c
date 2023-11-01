f2024bfcc15f005804985efcc7a28d765197c225
MeghanaSattim <147579237+MeghanaSattim@users.noreply.github.com>
2023-11-01 09:30:52 +0530

    #include<stdio.h> int main() {     int N;     scanf("%d",&N);     if (N%5==0 && N%3==0)     {         printf("PlingPlang");     }     else if (N%3==0)     {         printf("Pling");     }     else if (N%5==0)     {         printf("Plang");     }     else if (N%7==0)     {         printf("Plong");     }     else     {         printf("%d",N);     } }

---
+++ RainDrop.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int N;
+    scanf("%d",&N);
+    if (N%5==0 && N%3==0)
+    {
+        printf("PlingPlang");
+    }
+    else if (N%3==0)
+    {
+        printf("Pling");
+    }
+    else if (N%5==0)
+    {
+        printf("Plang");
+    }
+    else if (N%7==0)
+    {
+        printf("Plong");
+    }
+    else 
+    {
+        printf("%d",N);
+    }
+}
