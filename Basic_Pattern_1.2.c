d841d6b3b3e3225b5e6e25cc51b5774ecd489042
MeghanaSattim <147579237+MeghanaSattim@users.noreply.github.com>
2023-11-07 12:09:19 +0530

    #include<stdio.h> int main() {     int i,j,N;     scanf("%d",&N);     for (j=1;j<=N;j++)     {         for (i=1;i<=N;i++)         {             printf("%d ",i);         }         printf(" ");     } }

---
+++ Basic_Pattern_1.2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,N;
+    scanf("%d",&N);
+    for (j=1;j<=N;j++)
+    {
+        for (i=1;i<=N;i++)
+        {
+            printf("%d ",i);
+        }
+        printf("
+");
+    }
+}
