34a51873d17737054df702518af07a3b8fe86c91
MeghanaSattim <147579237+MeghanaSattim@users.noreply.github.com>
2023-11-28 04:47:12 +0530

    #include<stdio.h> int main() {     int  i,A,B;     scanf("%d%d",&A,&B);     for(i=A;i<=B;i++)     {         if (i%2==0)         {             printf("%d ",i);
            }
            
        }
    }

---
+++ Print_even_numbers_up_to_a_given_number.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int  i,A,B;
+    scanf("%d%d",&A,&B);
+    for(i=A;i<=B;i++)
+    {
+        if (i%2==0)
+        {
+            printf("%d ",i);
+        
+        }
+        
+    }
+}
