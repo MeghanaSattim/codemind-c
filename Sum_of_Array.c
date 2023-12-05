ba728a6a7c19b8edcabb2f4205dbdb843907a5f7
MeghanaSattim <147579237+MeghanaSattim@users.noreply.github.com>
2023-12-05 15:02:34 +0530

    #include<stdio.h> int main() { int N; scanf("%d",&N); int arr[N]; for (int i=0;i<N;i++) {     scanf("%d",&arr[i]); } int sum=0; for ( int i=0;i<N;i++) {     sum=sum+arr[i]; } printf("%d",sum); }

---
+++ Sum_of_Array.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int N;
+scanf("%d",&N);
+int arr[N];
+for (int i=0;i<N;i++)
+{
+    scanf("%d",&arr[i]);
+}
+int sum=0;
+for ( int i=0;i<N;i++)
+{
+    sum=sum+arr[i];
+}
+printf("%d",sum);
+}
