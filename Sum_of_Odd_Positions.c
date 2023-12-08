0234ad3a05208007841ef3e2e2cbecf9dc2cc578
MeghanaSattim <147579237+MeghanaSattim@users.noreply.github.com>
2023-12-08 06:06:18 +0530

    #include<stdio.h> int main() {     int N;     scanf("%d",&N);     int arr[N];     for (int i=0;i<N;i++)     {         scanf("%d",&arr[i]);     }     int i, sum=0;     for (i=0;i<N;i++)     {         if (i%2!=0)         {             sum=sum+arr[i];         }     }     printf("%d",sum); }

---
+++ Sum_of_Odd_Positions.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int N;
+    scanf("%d",&N);
+    int arr[N];
+    for (int i=0;i<N;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int i, sum=0;
+    for (i=0;i<N;i++)
+    {
+        if (i%2!=0)
+        {
+            sum=sum+arr[i];
+        }
+    }
+    printf("%d",sum);
+}
