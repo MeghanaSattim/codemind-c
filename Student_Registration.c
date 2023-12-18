f52d6fffc52f03220a6c36f8593a6af174498a25
MeghanaSattim <147579237+MeghanaSattim@users.noreply.github.com>
2023-12-18 09:51:43 +0530

    #include<stdio.h> int main() {     int T;     scanf("%d",&T);     int N,M,K;     scanf("%d%d%d",&N,&M,&K);     if (N<(M-K))     printf("YES ");     else     printf("NO ");     int n,m,k;     scanf("%d%d%d",&n,&m,&k);     if(n<(m-k))     printf("YES ");     else     printf("NO "); }

---
+++ Student_Registration.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int T;
+    scanf("%d",&T);
+    int N,M,K;
+    scanf("%d%d%d",&N,&M,&K);
+    if (N<(M-K))
+    printf("YES
+");
+    else
+    printf("NO
+");
+    int n,m,k;
+    scanf("%d%d%d",&n,&m,&k);
+    if(n<(m-k))
+    printf("YES
+");
+    else
+    printf("NO
+");
+}
