aeef12bcdec77f90e92229e88686adf94453bf47
MeghanaSattim <147579237+MeghanaSattim@users.noreply.github.com>
2023-12-19 11:53:17 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     int arr[n];     for (i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     int sum=0;     for (i=0;i<n;i++)     {         sum=sum+arr[i];     }     int avg=sum/n;     int count=0;     for (int i=0;i<n;i++)     {         if (arr[i]<=avg)         {             count++;         }     }     printf("%d",count); }

---
+++ Elements_smaller_than_average.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    int arr[n];
+    for (i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int sum=0;
+    for (i=0;i<n;i++)
+    {
+        sum=sum+arr[i];
+    }
+    int avg=sum/n;
+    int count=0;
+    for (int i=0;i<n;i++)
+    {
+        if (arr[i]<=avg)
+        {
+            count++;
+        }
+    }
+    printf("%d",count);
+}
